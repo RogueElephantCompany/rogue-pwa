e52e825ccb2910a3c934dc757eac0256c5f0c312
nangelos <nicksangelos@gmail.com>
2018-08-07 12:14:45 -0500

    [previous] see details of previous repairs

--- client/components/user/previous.js
+++ client/components/user/previous.js
@@ -14,40 +14,81 @@ const getPreviousAppts = () => {
 class PreviousAppointments extends Component {
   state = {
     priorAppts: prevAppts,
-    active: ''
+    selected: ''
   }
 
   componentDidMount() {
     console.log('Fetch Previous Appointments')
   }
 
+  seeDetails = (date) => {
+    if (date === this.state.selected) {
+      this.setState({ selected: '' })
+    }
+    else {
+      this.setState({ selected: date })
+    }
+  }
+
+  hideDetails = () => {
+    this.setState({ selected: '' })
+  }
 
 
   render() {
-    const { priorAppts } = this.state;
+    const { priorAppts, selected } = this.state;
     return (
       <div>
-        <h1 style={{ 'text-align': 'center', color: 'rgb(21, 39, 155)' }}>Previous Repairs </h1>
+        <h1 style={{ 'textAlign': 'center', color: 'rgb(21, 39, 155)' }}>Previous Repairs </h1>
         <div className="previous-component">
           <div className="column-heads">
             <h2 id="date-col">Date</h2>
             <h2 id="repair-col">Repair</h2>
-            <h2 id="repair-col">Cost</h2>
+            <h2 id="cost-col">Cost</h2>
           </div>
           {
             priorAppts.map(appt =>
               (
-                <div key={appt.date} className="prev-appt">
+                <button key={appt.date}
+                  type="submit"
+                  className="prev-appt"
+                  onClick={() => this.seeDetails(appt.date)}>
                   <h3 className="prev-date">{appt.date}</h3>
                   <h3 className="prev-problem">{appt.problem}</h3>
-                  {/* <h3 className="prev-tech">{appt.technician}</h3> */}
-                  {/* <h3 className="prev-time">{appt.apptLength}</h3> */}
                   <h3 className="prev-cost">{appt.cost}</h3>
-                </div>
+                </button>
               )
             )
           }
         </div>
+        {
+          selected !== '' ? (
+            <div>
+              <h1 style={{ 'textAlign': 'center', color: 'rgb(21, 39, 155)', marginTop: '10px' }}>Details </h1>
+              {
+                priorAppts.filter(appt => {
+                  return appt.date === selected
+                })
+                  .map(appt => (
+                    <div key={appt.date} className="previous-details">
+                      <h1>Date: {appt.date}</h1>
+                      <h1>Repair: {appt.problem}</h1>
+                      <h1>Appt Length: {appt.apptLength}</h1>
+                      <h1>Technician: {appt.technician}</h1>
+                      <h1>Cost: {appt.cost}</h1>
+                    </div>
+                  ))
+              }
+              <div style={{ textAlign: 'center', marginTop: '20px' }}>
+                <button
+                  id="signin-button"
+                  type='submit'
+                  onClick={this.hideDetails}>Hide </button>
+              </div>
+            </div>
+          ) : ''
+        }
+
       </div>
     )
   }
