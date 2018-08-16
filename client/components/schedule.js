f22032edc490992fdd725f03f1e710b5ed0e3ae4
nangelos <nicksangelos@gmail.com>
2018-08-16 10:42:14 -0500

    code cleanup

--- client/components/schedule.js
+++ client/components/schedule.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import Calendar from './calendar'
+import {url} from '../../secrets'
 // import {render} from 'react-dom'
 // import moment from 'moment'
 
@@ -8,22 +9,17 @@ import Calendar from './calendar'
 // BigCalendar.momentLocalizer(moment)
 // require('style-loader!css-loader!react-big-calendar/lib/css/react-big-calendar.css')
 
-const key = 'AIzaSyC2WyQfbYX2crwfplDh1GxmTGwhRey8k1k'
-const id = 'ef489024f184ab14c70ae7653868e070ab592c32'
-
 class Schedule extends Component {
   state = {
     now: ''
   }
-
-  url = 'https://calendar.google.com/calendar/embed?src=fixiteddie.com_c431af3jv545tkdvc7sb22oc6g%40group.calendar.google.com&ctz=America%2FChicago'
 
   render() {
     return (
       <div>
         <h1>Here is the schedule!</h1>
         <iframe
-          src={this.url}
+          src={url}
           style={{
             border: '0',
             width: '100vw',
